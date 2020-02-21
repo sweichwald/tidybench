e657080ca1c518a3d455d5dddae2a26eab359868
gherardovarando <gherardo.varand@gmail.com>
2020-02-21 10:17:34 +0100

    Clean up selvar
    Also fix flake8.

--- tidybench/selvar.py
+++ tidybench/selvar.py
@@ -5,67 +5,68 @@ Based on an implementation that is originally due to Gherardo Varando
 (gherardovarando).
 """
 
-
-import numpy as np
+from .utils import common_pre_post_processing
 ###
-# compile selvar.f with:
-#    f2py -llapack -c -m selvar selvar.f
+# compile selvarF.f with:
+#    f2py -llapack -c -m selvarF selvarF.f
 ###
 try:
-    from selvarF import slvar, gtstat, gtcoef
-    from scipy.stats import chi2
+    from .selvarF import slvar, gtstat, gtcoef
 except ImportError:
     slvar = None
     gtstat = None
     gtcoef = None
-    chi2 = None
 
 
-stats = ["DF", "LR", "FS"]
-coefs = ["ABS", "SQR", "COEF"]
-
-
-def interpnans(y):
-    nans = np.isnan(y)
-
-    def x(z):
-        return z.nonzero()[0]
-
-    y[nans] = np.interp(x(nans), x(~nans), y[~nans])
-    return y
-
-
+@common_pre_post_processing
 def selvar(data,
            maxlags=1,
            batchsize=-1,
-           score="default",
-           nrm=1,
            mxitr=-1,
-           trace=0):
+           trace=0,
+           ):
+    """
+    SELVAR (Selective auto-regressive model).
+
+    Parameters
+    ----------
+    data : ndarray
+        T (timepoints) x N (variables) input data
+
+    maxlags : int
+        Maximum number of lags to include in the model.
+        If maxlags < 0 then the maximum lag will be iteratively
+        adjusted for each variable until no decrease in PRSS.
+
+    batchsize : int
+        Number of consecutive time points to use in each training batch.
+        If batchsize < 0 then batchsize is set to the maximum available
+        time boints given maxlags.
+
+    mxitr : int
+        Maximum number of iterations (each variable) for the
+        hill-climbing search. If mxitr < 0 then the
+        search will stop only when no decrease in PRSS is possible.
+
+    trace : int
+        If positive messages will be printed out during the search.
+
+    Arguments for the common pre-processing steps of the data and the common
+    post-processing steps of the scores are documented in
+    utils.common_pre_post_processing
+
+    Returns
+    ----------
+    scores : ndarray
+        Array with scores for each link i -> j
+    """
 
     if slvar is None:
         raise RuntimeError("selvarF.f is not yet compiled")
 
-    # replace missing values coded as 999
-    data[data == 999] = np.nan
-    for j in range(data.shape[1]):
-        data[:, j] = interpnans(data[:, j])
+    scores, lags, info = slvar(data, bs=int(batchsize), ml=int(maxlags),
+                               mxitr=int(mxitr), trc=int(trace))
+    scores, df = gtstat(data, a=lags, bs=int(batchsize), ml=int(maxlags),
+                        job="DF")
 
-    ml = int(maxlags)
-
-    scores, a, info = slvar(data, bs=batchsize, ml=ml, mxitr=mxitr, trc=trace)
-    stat, df = gtstat(data, a=a, bs=batchsize, ml=ml, job="LR")
-
-    pvalues = 1 - chi2.cdf(stat, df[:, 0] - df[:, 1])
-    pvalues = pvalues * data.shape[1]
-    pvalues[pvalues > 1] = 1
-    lags = a
-
-    if score in stats:
-        scores, df = gtstat(data, a=a, bs=batchsize, ml=ml, job=score)
-    if score in coefs:
-        scores, info = gtcoef(
-            data, a=a, bs=batchsize, ml=ml, job=score, nrm=nrm)
-    scores = abs(scores)
-
-    return scores, pvalues, lags
+    return scores
